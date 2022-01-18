088a4f1a7ee8e92cc4e9bd21228453ccec1c36f4
Fevereiro <87093620+fevereiroo00@users.noreply.github.com>
2022-01-18 17:06:11 +0000

    Add files via upload

---
+++ Fevereiro_tabuadas/Fevereiro_tabuadas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fevereiro_tabuadas
+{
+    class Program
+    {
+        static int n;
+        
+        static void Main(string[] args)
+        {
+
+            int v = 1;
+            string d;
+
+            while(v != 0)
+            {
+                Console.WriteLine("Digi-te um numero. Esse numero vai ser feito para formar a respetiva tabuada");
+                n = int.Parse(Console.ReadLine());
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine("Tabuada do" + n);
+
+                    Console.WriteLine(n + "*" + i + "="+n*i);
+                }
+                Console.WriteLine("Prentende continuar ?");
+                d = Console.ReadLine();
+
+                if (d == "Sim" )
+                {
+
+                }
+                else if (d == "Não")
+                {
+                    Environment.Exit(0);
+                }
+
+
+
+
+            }
+            
+
+        }
+    }
+}
